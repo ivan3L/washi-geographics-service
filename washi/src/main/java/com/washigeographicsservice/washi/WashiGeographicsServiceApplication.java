7b5df88829177e474ee9fd14452322c35087d136
ivan3L <63982011+ivan3L@users.noreply.github.com>
2021-05-08 15:46:12 -0500

    initial commit

---
+++ washi/src/main/java/com/washigeographicsservice/washi/WashiGeographicsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.washigeographicsservice.washi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashiGeographicsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashiGeographicsServiceApplication.class, args);
+	}
+
+}
---
+++ washi/src/test/java/com/washigeographicsservice/washi/WashiGeographicsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washigeographicsservice.washi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashiGeographicsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
